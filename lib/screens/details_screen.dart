2b5a8c0fc6a74de9b1e0c92861841239acf6c799
Kwame Agyapong <Kwameagyapong91@gmail.com>
2023-11-20 17:58:03 +0000

    final build

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,11 +1,123 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:movie_app/colors.dart';
+import 'package:movie_app/constants.dart';
 import 'package:movie_app/models/trending_movie.dart';
+import 'package:movie_app/widgets/back_button.dart';
 
 class DetailScreen extends StatelessWidget {
   const DetailScreen({super.key, required this.movie});
   final TrendingMovie movie;
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar.large(
+            leading: const ButtonBack(),
+            backgroundColor: Colours.scaffoldBgColor,
+            expandedHeight: 500,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(movie.title,
+                  style: GoogleFonts.roboto(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  )),
+              background: ClipRRect(
+                borderRadius: BorderRadius.circular(25),
+                child: Image.network(
+                  '${Constants.imagePath}${movie.posterPath}',
+                  fit: BoxFit.cover,
+                  filterQuality: FilterQuality.high,
+                ),
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(14.0),
+              child: Column(
+                children: [
+                  Text('Overview',
+                      style: GoogleFonts.roboto(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  const SizedBox(
+                    height: 17,
+                  ),
+                  Text(movie.overview,
+                      style: GoogleFonts.openSans(
+                        fontSize: 25,
+                        fontWeight: FontWeight.w400,
+                      )),
+                  const SizedBox(
+                    height: 17,
+                  ),
+                  SizedBox(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.grey),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Row(
+                            children: [
+                              Text(
+                                'Release Date :',
+                                style: GoogleFonts.openSans(
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                              Text(
+                                movie.releaseDate,
+                                style: GoogleFonts.openSans(
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.grey),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Row(
+                            children: [
+                              Text(
+                                'Rating: ',
+                                style: GoogleFonts.openSans(
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                              const Icon(
+                                Icons.star,
+                                color: Colors.amber,
+                              ),
+                              Text(
+                                '${movie.voteAverage.toStringAsFixed(1)}/10',
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/widgets/custom_scroll_widget.dart
+++ lib/widgets/custom_scroll_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:movie_app/constants.dart';
+import 'package:movie_app/screens/details_screen.dart';
 
 class CustomScrollWidget extends StatelessWidget {
   const CustomScrollWidget({super.key, required this.snapshot});
@@ -17,15 +18,25 @@ class CustomScrollWidget extends StatelessWidget {
           itemBuilder: (context, index) {
             return Padding(
               padding: const EdgeInsets.all(8),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(8),
-                child: SizedBox(
-                    height: 200,
-                    width: 150,
-                    child: Image.network(
-                        filterQuality: FilterQuality.high,
-                        fit: BoxFit.cover,
-                        '${Constants.imagePath}${snapshot.data![index].posterPath}')),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => DetailScreen(
+                                movie: snapshot.data[index],
+                              )));
+                },
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: SizedBox(
+                      height: 200,
+                      width: 150,
+                      child: Image.network(
+                          filterQuality: FilterQuality.high,
+                          fit: BoxFit.cover,
+                          '${Constants.imagePath}${snapshot.data![index].posterPath}')),
+                ),
               ),
             );
           },
